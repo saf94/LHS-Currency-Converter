03dc1cd5533770d5b5139e621ef7b58368ace397
Safeer Hayat <SAFHAYAT@Safeers-Air.home>
2018-04-07 16:42:24 +0100

    finishing touches

--- lhs-currency-converter/src/App.js
+++ lhs-currency-converter/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import Home from './Containers/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       <Home /> 
-      </div>
     );
   }
 }
